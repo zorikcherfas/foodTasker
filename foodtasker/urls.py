abceaa7e072f3776e94e0064939b19abcb2dc21e
Zorik Cherfas <zorik.cherfas@gmail.com>
2017-06-26 08:38:38 +0300

    Done with order report dashboard

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -34,13 +34,23 @@ urlpatterns = [
     url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
     # /convert-token (sign in/ sign up)
     # /revoke-token (sign out)
-
-    #API for restaurant
     url(r'^api/restaurant/order/notification/(?P<last_request_time>.+)/$', apis.restaurant_order_notification),
-    # APIS for customers
-    url(r'^api/customer/restaurant/$', apis.customer_get_restaurant),
-    url(r'^api/customer/meals/(?P<restaurant_id>\d+)$', apis.customer_get_meals),
+
+
+    # APIs for CUSTOMERS
+    url(r'^api/customer/restaurants/$', apis.customer_get_restaurants),
+    url(r'^api/customer/meals/(?P<restaurant_id>\d+)/$', apis.customer_get_meals),
     url(r'^api/customer/order/add/$', apis.customer_add_order),
-    url(r'^api/customer/order/lastes/$', apis.customer_get_lasts_order),
+    url(r'^api/customer/order/latest/$', apis.customer_get_latest_order),
+    url(r'^api/customer/driver/location/$', apis.customer_driver_location),
+
+    #APIs for Drivers
+    url(r'^api/driver/orders/ready/$', apis.driver_get_ready_orders),
+    url(r'^api/driver/orders/pick/$', apis.driver_pick_order),
+    url(r'^api/driver/orders/lasts/$', apis.driver_get_latest_order),
+    url(r'^api/driver/orders/complete/$', apis.driver_complete_order),
+    url(r'^api/driver/reveneu/$', apis.driver_get_revenue),
+    url(r'^api/driver/location/update/$', apis.driver_update_location),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -1,12 +1,17 @@
 import json
 from django.http import JsonResponse
-from foodtaskerapp.models import Restaurant , Meal , Order, OrderDetails
-from foodtaskerapp.serializers import RestaurantSerializer ,MealSerializer
+from foodtaskerapp.models import Restaurant , Meal , Order, OrderDetails , Driver
+from foodtaskerapp.serializers import RestaurantSerializer ,MealSerializer , OrderSerializer
 from oauth2_provider.models import AccessToken
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
-def customer_get_restaurant(request):
+
+
+###############
+# Customer
+###############
+def customer_get_restaurants(request):
     restaurants = RestaurantSerializer(
         Restaurant.objects.all().order_by("-id"),
         many=True,
@@ -87,15 +92,169 @@ def customer_add_order(request):
 
     })
 
-def customer_get_lasts_order(request):
 
-    return JsonResponse({
+def customer_get_latest_order(request):
+    # access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        # expires__gt = timezone.now())
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+    expires__gt = timezone.now())
 
-    })
+    customer = access_token.user.customer
+    order = OrderSerializer(Order.objects.filter(customer = customer).last()).data
 
-def restaurant_order_notification(request , last_request_time):
+    return JsonResponse({"order": order})
 
+###############
+# Restaurant
+###############
+def restaurant_order_notification(request, last_request_time):
     notification = Order.objects.filter(restaurant = request.user.restaurant,
-    created_at__gt = last_request_time).count();
+        created_at__gt = last_request_time).count()
 
-    return JsonResponse({"notification":notification})
+    return JsonResponse({"notification": notification})
+
+def customer_driver_location(request):
+
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        expires__gt=timezone.now())
+
+    customer = access_token.user.customer
+    print(customer)
+    curret_order = Order.objects.filter(customer = customer , status = Order.ONTHEWAY).last()
+    print(curret_order)
+    location = curret_order.driver.location
+
+    return JsonResponse({"location":location})
+
+
+###############
+# Restaurant
+#############
+def driver_get_ready_orders(request):
+    orders = OrderSerializer(
+        Order.objects.filter(status = Order.READY ,driver =None,).order_by("-id"), many=True
+        ).data
+
+    return JsonResponse({"orders":orders})
+
+@csrf_exempt
+def driver_pick_order(request):
+    if request.method == "POST":
+        # Get token
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt = timezone.now())
+
+    # Get Driver
+
+        driver = access_token.user.driver
+
+    #Check if driver an only pick one order in the same timezone
+    if Order.objects.filter(driver = driver).exclude(status = Order.ONTHEWAY):
+        return JsonResponse({"status":"failed" ,"Error": "You can only pick up one item at the same time"})
+
+    try:
+        order = Order.objects.get(
+            id = request.POST["order_id"],
+            driver = None,
+            status = Order.READY
+        )
+        order.driver = driver
+        order.status = Order.ONTHEWAY
+        order.picked_at = timezone.now()
+        order.save()
+
+        return JsonResponse({"status":"success"})
+
+    except Order.DoesNotExist:
+        return JsonResponse({"status":"falied","error":"This order has been picked up by other driver"})
+
+    return JsonResponse({})
+
+
+def driver_get_latest_order(request):
+
+    try:
+        access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+            expires__gt = timezone.now())
+
+    except AccessToken.DoesNotExist:
+        return JsonResponse ({"status":"falied","error":"access_token is wrong"})
+
+    driver = access_token.user.driver
+    order = OrderSerializer (
+        Order.objects.filter(driver = driver).order_by("picked_at").last()
+    ).data
+
+    return JsonResponse({"order":order })
+
+@csrf_exempt
+def driver_complete_order(request):
+
+    try:
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt=timezone.now())
+    except AccessToken.DoesNotExist:
+        return JsonResponse ({"status":"falied","error":"access_token is wrong"})
+
+    driver = access_token.user.driver
+
+    order = Order.objects.get(id = request.POST['order_id'] , driver =driver)
+
+    order.status  = order.DELIVERED
+    order.save()
+
+    return JsonResponse({"status":"success"})
+
+
+#POST params: access_token , order_id
+@csrf_exempt
+def driver_get_revenue(request):
+
+    try:
+        # print(request.POST.get('access_token')
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt=timezone.now())
+    except AccessToken.DoesNotExist:
+        return JsonResponse ({"status":"falied","error":"access_token is wrong"})
+
+    driver = access_token.user.driver
+
+    from datetime import timedelta
+
+    reveneu = {}
+    today = timezone.now()
+    current_weekdays = [
+        today + timedelta(days = i) for i in range(0-today.weekday(), 7 - today.weekday())
+    ]
+
+
+    for day in current_weekdays:
+        print(day)
+        orders = Order.objects.filter(
+            driver = driver,
+            status = Order.DELIVERED,
+            created_at__year = day.year,
+            created_at__month = day.month,
+            created_at__day = day.day
+        )
+
+        reveneu[day.strftime("%a")] = sum(order.total for order in orders)
+
+    return JsonResponse({"reveneu":reveneu})
+
+#POST params: access_token , string "lat,lng"
+@csrf_exempt
+def driver_update_location(request):
+
+    if request.method == "POST":
+
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt=timezone.now())
+
+        driver = access_token.user.driver
+        driver.location = request.POST["location"]
+        driver.save()
+
+        return JsonResponse({})
+
+    return JsonResponse({"status":"fail"})
